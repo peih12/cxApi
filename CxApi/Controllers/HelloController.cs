fa72fc0df474a5245e24fd05339beae6aca0151a
Hiep Nguyen <peih12@gmail.com>
2017-05-21 13:48:11 +0200

    done

--- CxApi/Controllers/HelloController.cs
+++ CxApi/Controllers/HelloController.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using CxApi.Models;
+using CxModel;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CxApi.Controllers
